e528dd30c751c13e94f7bdde02ef6ca651dd3fba
Gouldilocks <cagould@smu.edu>
2022-05-05 00:10:39 -0500

    update

--- face_recognize.py
+++ face_recognize.py
@@ -2,6 +2,22 @@ import numpy as np
 import face_recognition as fr
 import cv2
 import os
+import dlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Importing Deep Learning Libraries
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Input, Dropout, GlobalAveragePooling2D, Flatten, Conv2D, BatchNormalization, Activation, MaxPooling2D
+from keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam, SGD, RMSprop
+
+picture_size = 48
+folder_path = ""
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("./models/shape_predictor_68_face_landmarks.dat")
 
 video_capture = cv2.VideoCapture(0)
 # loop over all files in directory "people"
@@ -22,9 +38,11 @@ for filename in os.listdir("people"):
         print("error: ", e)
         print("no face found in ", filename)
         continue
-    
+ 
 while True:
     ret, frame = video_capture.read()
+    gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
 
     rgb_frame = frame[:, :, ::-1]
 
@@ -33,6 +51,23 @@ while True:
 
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
 
+        for face in faces:
+            x1 = face.left()  # left point
+            y1 = face.top()  # top point
+            x2 = face.right()  # right point
+            y2 = face.bottom()  # bottom point
+
+            # Look for the landmarks
+            landmarks = predictor(image=gray, box=face)
+
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+
+                # Draw a circle
+                cv2.circle(img=frame, center=(x, y), radius=2,
+                        color=(0, 255, 0), thickness=-1)
+ 
         matches = fr.compare_faces(known_face_encodings, face_encoding)
 
         name = "Unknown Face"
